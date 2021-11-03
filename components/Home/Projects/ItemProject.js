71e344fc045d9be473620d48d7181f1fcd276257
distordev <adanuriplata@gmail.com>
2021-11-03 01:45:07 -0600

    Fix: tag target blank

--- components/Home/Projects/ItemProject.js
+++ components/Home/Projects/ItemProject.js
@@ -27,7 +27,7 @@ const ItemProject = ({ name, description, technologies, image, siteurl }) => {
       </div>
       <div className="flex justify-end mt-4">
         <div className="px-4">
-          <a href={siteurl} target="_BLANK">
+          <a href={siteurl} target="_BLANK" rel="noreferrer">
             <ArrowLinkIcon className=" w-8 fill-current text-white-ddev" />
           </a>
         </div>
