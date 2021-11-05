02ea370389edc7c5b7a8189a4cbbbbfe4cf49937
distordev <adanuriplata@gmail.com>
2021-11-05 00:34:21 -0600

    Fix: class for classname

--- components/globals/heros/MainHero.js
+++ components/globals/heros/MainHero.js
@@ -2,13 +2,13 @@ import React from "react";
 
 const MainHero = ({ title }) => {
   return (
-    <header class=" mb-8 py-5 bg-gray-900">
-      <div class="max-w-screen-xl mx-auto px-4 md:px-16 lg:px-32">
+    <header className=" mb-8 py-5 bg-gray-900">
+      <div className="max-w-screen-xl mx-auto px-4 md:px-16 lg:px-32">
         <h1
-          class="text-white-l-ddev md:text-4xl text-3xl"
+          className="text-white-l-ddev md:text-4xl text-3xl"
           style={{ fontFamily: "'DotGothic16'" }}
         >
-          <span class="text-green-ddev font-mono text-3xl">&gt; </span>
+          <span className="text-green-ddev font-mono text-3xl">&gt; </span>
           {title} _
         </h1>
       </div>
