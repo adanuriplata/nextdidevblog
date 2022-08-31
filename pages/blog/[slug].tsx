0929c47bd0330ced77fdb788ca9d9cbe339a85b1
adanplata <adanuriplata@gmail.com>
2022-08-30 23:58:57 -0500

    feat: Added PostCard Ui

---
+++ pages/blog/[slug].tsx
@@ -0,0 +1,9 @@
+
+
+export const Post = () => {
+    return (
+        <h1>Hola post </h1>
+    )
+}
+
+export default Post;
---
+++ pages/blog/index.tsx
@@ -0,0 +1,24 @@
+import { ReactElement } from "react";
+import MainLayout from "../../components/MainLayout";
+import { PostCard } from "../../components/ui";
+
+
+const Blog = () => {
+  return (
+      <div className="mt-5 mb-10 grid grid-cols-1 sm:grid-cols-2 gap-6 lg:grid-cols-3">
+        {
+          [1,2,3,4,5,6,7].map((item, index) => <PostCard key={index} />)
+        }        
+      </div>
+  );
+};
+
+export default Blog;
+
+Blog.getLayout = function getLayout(Contacto: ReactElement) {
+  return (
+    <MainLayout heroTitle="Blog" >
+      {Contacto}
+    </MainLayout>
+      ) 
+}
