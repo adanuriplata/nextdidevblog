0489dd30a2b3af18273ee8cffcc9f131fff168f3
distordev <adanuriplata@gmail.com>
2022-09-02 16:28:14 -0500

    Feat: Added next seo config

--- components/MainLayout/AppMeta.tsx
+++ components/MainLayout/AppMeta.tsx
@@ -1,9 +1,22 @@
 import React from "react";
 import Head from "next/head";
-import {DefaultSeo} from 'next-seo'
-import SEO from '../../utils/next-seo.config';
+import { NextSeo } from 'next-seo'
+import { AppConfig } from '../../utils/AppConfig';
+import { useRouter } from "next/router";
 
-const AppMeta = () => {
+interface AppMetaProps {
+  title?: string,
+  description?: string,
+  canonical?: string,
+  cover?: string
+}
+
+const AppMeta = ({ title, description, canonical, cover }: AppMetaProps) => {
+
+  const site = process.env.NEXT_PUBLIC_HOST
+  const URL = site + useRouter().pathname;
+
+
   return (
     <>
       <Head>
@@ -18,7 +31,44 @@ const AppMeta = () => {
           href='https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/github-dark.min.css'
         />
       </Head>
-      <DefaultSeo {...SEO}/>
+      
+      <NextSeo
+        title= {title ? title + '_' + AppConfig.site_name : AppConfig.site_name}
+        description={description ? description : AppConfig.description}
+        canonical={canonical ? canonical : URL}
+        openGraph={{
+          url: URL,
+          title: title ? title + '_' + AppConfig.site_name : AppConfig.site_name,
+          description: description ? description : AppConfig.description,
+          images: [
+            {
+              url: 'https://www.example.ie/og-image-01.jpg',
+              width: 800,
+              height: 600,
+              alt: 'Og Image Alt',
+              type: 'image/jpeg',
+            },
+            {
+              url: 'https://www.example.ie/og-image-02.jpg',
+              width: 900,
+              height: 800,
+              alt: 'Og Image Alt Second',
+              type: 'image/jpeg',
+            },
+            { url: 'https://www.example.ie/og-image-03.jpg' },
+            { url: 'https://www.example.ie/og-image-04.jpg' },
+          ],
+          site_name: AppConfig.site_name,
+        }}
+        twitter={{
+          handle: '@handle',
+          site: AppConfig.username,
+          cardType: 'summary_large_image',
+        }}
+      />
+
+
+
     </>
   );
 };
---
+++ utils/AppConfig.ts
@@ -0,0 +1,7 @@
+export const AppConfig = {
+  site_name: '👨‍💻 AdanPlata',
+  title: 'AdanPlata',
+  description: '👨‍💻 FronEnd Developer #JavaScript | 📈 Growth Marketer #GoogleAds | 🎸 Guitarist | 🚴 Fixie Rider | 🏃 Runner',
+  locale: 'es',
+  username: '@adanuriplata'
+};
