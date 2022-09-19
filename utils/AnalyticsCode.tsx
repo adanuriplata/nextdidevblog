4069669ca722f07e008d9db1ba1f1f732dfbcf09
distordev <adanuriplata@gmail.com>
2022-09-19 16:36:10 -0500

    Feat: Added Analytics basic code

---
+++ utils/AnalyticsCode.tsx
@@ -0,0 +1,18 @@
+export const AnalyticsCode: JSX.Element = ( <>
+  <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      /> 
+      <script
+      dangerouslySetInnerHTML={{
+        __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+          page_path: window.location.pathname,
+        });
+      `,
+      }}
+    />
+</>)
